bd9cdab940954186b6e28d9a805e529b7d2d54ba
Lemonsandlimez <anthonyjrgames@gmail.com>
2026-05-11 12:46:44 -0400

    Update ApiTime.py

--- Examples/ApiTime.py
+++ Examples/ApiTime.py
@@ -1,18 +1,13 @@
-# imports
-
 from Core.Router import Router
 from Core.Server import Run, Server
 from Core.Json import Json
 import time
 
-router = Router() # start router
-
-# Add a Route
+router = Router()
 
 @router.AddRoute("/api/time")
 def Time(req):
     return Json({"server_time": time.ctime()})
 
-# Run
 server = Server(router, Port=8080)
 Run(server)
