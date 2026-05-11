4ae6b9f4010da49b95f2640412824cb4f2de8337
Lemonsandlimez <anthonyjrgames@gmail.com>
2026-05-11 12:41:49 -0400

    comment HelloUser.py

--- Examples/HelloUser.py
+++ Examples/HelloUser.py
@@ -1,12 +1,18 @@
+# imports
+
 from Core.Router import Router
 from Core.Server import Run, Server
 from Core.Response import Response
 
-router = Router()
+router = Router() # start router
 
 @router.AddRoute("/hello/:name")
 def Hello(req, name):
-    return Response(f"<h1>Hello, {name}!</h1>", Headers={"Content-Type": "text/html"})
+    return Response(f"<h1>Hello, {name}!</h1>", Headers={"Content-Type": "text/html"}) # html 
+
+# run
 
 server = Server(router, Port=8080)
 Run(server)
+
+# Use with http://127.0.0.1:8080/hello/john when running
